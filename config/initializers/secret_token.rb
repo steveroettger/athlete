a56e7164db2dbb05ec3e1f1a29aa6f136be02da9
Steve Roettger <steve@steveroettger.com>
2012-06-18 11:19:38 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Athlete::Application.config.secret_token = 'de4d96505949ca457150ab581fc7fc5d78e6f7e63e9ade5d75d514a54c737d4900a703699e028565515636015fc3b8a62c41e95b315825aa146fe030385875d7'
