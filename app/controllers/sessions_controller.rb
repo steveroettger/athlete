a960a89e504a64584b8f4a369ea1c6b93140978d
Steve Roettger <steve@steveroettger.com>
2012-08-30 16:36:22 -0400

    updated routing on new fb session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
-    redirect_to user
+    redirect_to edit_user_path(current_user)
   end
 
   def destroy
